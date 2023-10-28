e120eaba2404f0ae590b1883997d426a21a5c6da
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-28 21:04:46 +0530

    updated

--- hoobank_webapp/src/components/Navbar.jsx
+++ hoobank_webapp/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        <li></li>
+      </ul>
+    </nav>
   )
 }
 
